c1ab3053e68328dff13270b43ca353db9d801280
roberto-fernandino <romfernandino@gmail.com>
2023-08-31 19:01:14 -0300

    Graphs not responsive, added lot of style for user page, still much work to do, fix urls in all buttons, add config, prova, simulados style and url in the sidebar, still need sidebar animation and responsive for mobile but made graph work without refresh with the most simple way possible in the fucking world lets fucking go

--- app/usuarios/views.py
+++ app/usuarios/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect
-from usuarios.funcs import NotaChart, NotaFilteredChart
+from usuarios.funcs import NotaChart, NotaFilteredChart, MediaQuery
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from usuarios.forms import AccountCreationForm
 from django.contrib import messages
 from materiais.models import ProvaCompleta
 from django.http import JsonResponse
+from usuarios.models import MediaGeral
+
 # Create your views here.
 
 
 # CACHE AQUI
 @login_required()
 def notas_graph(request):
-    data_mat, data_nat, data_lin, data_hum, months_mat, months_nat, months_lin, months_hum = NotaChart(request.user)
+    (
+        data_mat,
+        data_nat,
+        data_lin,
+        data_hum,
+        months_mat,
+        months_nat,
+        months_lin,
+        months_hum,
+    ) = NotaChart(request.user)
+    media_mat, media_nat, media_lin, media_hum = MediaQuery(request.user)
     context = {
         "data_mat": data_mat,
         "months_mat": months_mat,
@@ -22,44 +34,50 @@ def notas_graph(request):
         "months_lin": months_lin,
         "data_hum": data_hum,
         "months_hum": months_hum,
+        "media_mat": media_mat,
+        "media_nat": media_nat,
+        "media_lin": media_lin,
+        "media_hum": media_hum,
     }
     return render(request, "usuarios/notas-graph.html", context)
 
 
 @login_required()
 def filter_graph_time(request):
-    months = (request.GET.get('filter', None))
+    months = request.GET.get("filter", None)
     months = int(months) if months else None
     result = NotaFilteredChart(request.user, months)
     print(result)
-    data_mat = result['data_mat']
-    data_nat = result['data_nat']
-    data_lin = result['data_lin']
-    data_hum = result['data_hum']
-    months_mat = result['months_mat']
-    months_nat = result['months_nat']
-    months_lin = result['months_lin']
-    months_hum = result['months_hum']
+    data_mat = result["data_mat"]
+    data_nat = result["data_nat"]
+    data_lin = result["data_lin"]
+    data_hum = result["data_hum"]
+    months_mat = result["months_mat"]
+    months_nat = result["months_nat"]
+    months_lin = result["months_lin"]
+    months_hum = result["months_hum"]
     context = {
         "graph_mat": {"newLabels": months_mat, "newData": data_mat},
         "graph_nat": {"newLabels": months_nat, "newData": data_nat},
         "graph_lin": {"newLabels": months_lin, "newData": data_lin},
         "graph_hum": {"newLabels": months_hum, "newData": data_hum},
-
     }
     return JsonResponse(context)
+
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("usuarios:user")
     elif request.method == "POST":
-        email = request.POST['email']
-        password = request.POST['password']
+        email = request.POST["email"]
+        password = request.POST["password"]
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             return redirect("home:home")
-        return render(request, "usuarios/login.html", {"message": "Email ou senha inválidos"})
+        return render(
+            request, "usuarios/login.html", {"message": "Email ou senha inválidos"}
+        )
     return render(request, "usuarios/login.html")
 
 
@@ -76,10 +94,17 @@ def signup(request, *args, **kwargs):
             if auth_user is not None:
                 # Se o usuario foi criado com sucesso ja loga
                 login(request, auth_user)
+                MediaGeral.objects.create(
+                    usuario=request.user,
+                    media_matematica=0,
+                    media_ciencias_natureza=0,
+                    media_ciencias_humanas=0,
+                    media_linguagens=0,
+                )
                 return redirect("usuarios:signup-sucess")
         context = {"form": form}
         return render(request, "usuarios/signup.html", context)
-    
+
     form = AccountCreationForm()
     context = {
         "form": form,
