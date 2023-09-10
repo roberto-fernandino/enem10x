9438ded268e851749bc951be464f94711cdf4eb6
roberto-fernandino <romfernandino@gmail.com>
2023-09-10 19:51:40 -0300

    Made the forms to choose provas 200% most optimized

--- app/provas/forms.py
+++ app/provas/forms.py
@@ -1,32 +1,35 @@
+from collections.abc import Mapping
+from typing import Any
 from django import forms
+from django.forms.utils import ErrorList
 from materiais.models import Materia, Simulado
 
 # Forms down here
 
 class ProvaChoose(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(ProvaChoose, self).__init__(*args, **kwargs)
-        QUESTOES_CHOICE = [(i, str(i)) for i in range(5,46)]
-        MATERIAS_CHOICE = [(materia.id, str(materia)) for materia in (Materia.objects.all())]
-        SIMULADO_CHOICE = [(simulado.id, str(simulado)) for simulado in (Simulado.objects.all())]
+    QUESTOES_CHOICE = [(i, str(i)) for i in range(5,46)]
     
-        num_questoes = forms.ChoiceField(
-            choices=QUESTOES_CHOICE,
-            label='Quantas questoes voce quer de cada materia??',
-            widget=forms.Select(),)
-        simulados = forms.MultipleChoiceField(
-            choices=SIMULADO_CHOICE,
-            widget=forms.CheckboxSelectMultiple,
-            label='Quais simulados?',
-            required=False,
-        )
-        materias = forms.MultipleChoiceField(
-            choices=MATERIAS_CHOICE,
-            widget=forms.CheckboxSelectMultiple,
-            label='Quais materias?',
-            required=False,
-        )
-        tipo_prova = forms.CharField(
-            required=True
-        )
+    def __init__(self, *args, **kwargs) -> None:
+        super(ProvaChoose, self).__init__(*args, **kwargs)
+        self.fields['materias'].choices = [(materia.id, str(materia)) for materia in (Materia.objects.all())]
+        self.fields['simulados'].choices = [(simulado.id, str(simulado)) for simulado in (Simulado.objects.all())]
+
+    num_questoes = forms.ChoiceField(
+        choices=QUESTOES_CHOICE,
+        label='Quantas questoes voce quer de cada materia??',
+        widget=forms.Select()
+    )
+    simulados = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        label='Quais simulados?',
+        required=False,
+    )
+    materias = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        label='Quais materias?',
+        required=False,
+    )
+    tipo_prova = forms.CharField(
+        required=True
+    )
 
