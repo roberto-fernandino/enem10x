2c2d6640c71537477d2d57f703fe993ef04aa753
roberto-fernandino <romfernandino@gmail.com>
2023-09-10 19:52:34 -0300

    Added one new filter tag.

--- app/provas/templatetags/provatag.py
+++ app/provas/templatetags/provatag.py
@@ -7,3 +7,7 @@ register = template.Library()
 def opcao_em_letra(value):
     letras = 'a b c d e '.split()
     return letras[value]
+
+@register.filter()
+def key(d, key_name):
+    return d.get(key_name)
