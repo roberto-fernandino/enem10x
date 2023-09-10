ab83488bc7be71192d530efa922f9f645c60395a
roberto-fernandino <romfernandino@gmail.com>
2023-09-10 19:52:50 -0300

    renamed choose-prova view

--- app/provas/urls.py
+++ app/provas/urls.py
@@ -6,7 +6,7 @@ app_name = 'provas'
 
 # Urls 
 urlpatterns = [
-    path('', prova_choose, name='prova'),
+    path('', prova_choose, name='prova-choose'),
     path('respondida/', prova_respondida, name='prova-respondida'),
     path('completa/<int:prova_id>/', prova_completa, name='prova-completa'),
 ]
