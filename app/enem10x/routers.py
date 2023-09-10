c68f06e77bb13cd33e627a67b1ba3bd124b9da41
roberto-fernandino <romfernandino@gmail.com>
2023-09-10 19:50:00 -0300

    Added a more inteligent router

--- app/enem10x/routers.py
+++ app/enem10x/routers.py
@@ -5,8 +5,15 @@ class Router:
     def db_for_write(self, model, **hints):
         return "default"
 
-    def allow_relation(self, ob1, ob2, **hints):
-        return True
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Relations between objects are allowed if both objects are
+        in the primary/replica pool.
+        """
+        db_set = {"default", "replica"}
+        if obj1._state.db in db_set and obj2._stated.db in db_set:
+            return True
+        return None
     
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         return db == 'default'
