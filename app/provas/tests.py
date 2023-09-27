3c852ec95ed722b991ab5ae39ea057008c625d2c
roberto-fernandino <romfernandino@gmail.com>
2023-09-26 23:05:04 -0300

    fuck this fucking test

--- app/provas/tests.py
+++ app/provas/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from materiais.models import Materia
 from .funcs import define_proporcao_conteudos_provas
+
+
 # Create your tests here.
 class DefineProporcaoConteudosProvasTeste(TestCase):
-    databases = {'default'}
+    databases = {'default', 'replica'}
     def test_define_proporcao_conteudos_provas(self):
         materia = Materia.objects.get(id=1)
         num_questoes = 10
