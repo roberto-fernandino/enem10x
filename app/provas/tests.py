308dd082da3019e5cbdc76f1b85f9cf7baa3a97e
roberto-fernandino <romfernandino@gmail.com>
2023-10-30 12:41:21 -0300

    Initiated unittests made conteudo, submateria and materia testcases.

--- app/provas/tests.py
+++ app/provas/tests.py
@@ -1,15 +1,38 @@
 from django.test import TestCase
-from materiais.models import Materia
-from .funcs import define_proporcao_conteudos_provas
+from materiais.models import Materia, SubMateria, GrupoConteudo, Conteudo
 
 
 # Create your tests here.
-class DefineProporcaoConteudosProvasTeste(TestCase):
-    databases = {"default", "replica"}
+class MateriaTestCase(TestCase):
+    def setUp(self) -> None:
+        self.materia = Materia.objects.create(nome="Biologia")
 
-    def test_define_proporcao_conteudos_provas(self):
-        materia = Materia.objects.get(id=1)
-        num_questoes = 10
-        result = define_proporcao_conteudos_provas(materia, num_questoes)
-        self.assertIsNotNone(result)
-        self.assertIsInstance(result, dict)
+    def testMateriaNome(self):
+        self.assertEqual(self.materia.nome, "Biologia")
+
+
+class SubMateriaTestCase(TestCase):
+    def setUp(self) -> None:
+        self.materia = Materia.objects.create(nome="Biologia")
+        self.submateria = SubMateria.objects.create(
+            nome="Citologia", materia=self.materia
+        )
+
+    def testSubMateria(self):
+        self.assertEqual(self.materia, self.submateria.materia)
+        self.assertEqual(self.submateria.nome, "Citologia")
+
+
+class ConteudoTestCase(TestCase):
+    def setUp(self) -> None:
+        self.materia = Materia.objects.create(nome="Biologia")
+        self.sub_materia = SubMateria.objects.create(
+            nome="Citologia", materia=self.materia
+        )
+        self.conteudo = Conteudo.objects.create(
+            nome="Células Animais", sub_materia=self.sub_materia
+        )
+
+    def testConteudo(self):
+        self.assertEqual(self.conteudo.sub_materia, self.sub_materia)
+        self.assertEqual(self.conteudo.nome, "Células Animais")
