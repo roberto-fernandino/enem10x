4720deb67b9f19e0c4e41b78c456f7a2d90780a0
roberto-fernandino <romfernandino@gmail.com>
2023-10-29 22:21:27 -0300

    New "guide" to set up production architecture.

--- comands.sql
+++ comands.sql
@@ -1,8 +1,16 @@
+! pg_hba.conf
+host    replication     all             0.0.0.0/0               scram-sha-256
 
--- Cria slot de replicacao --
-SELECT pg_create_physical_replication_slot('slot_replication_name');
+! Conecte ao psql
+# psql -U admin -d enem
 
+! Rode
+create role repuser with replication password 'repuser';
+grant select on all tables in schema public to repuser;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO repuser; 
+alter default privileges in schema public grant select on tables to repuser;
+select pg_create_physical_replication_slot('master_replication_slot');
 
-pg_basebackup -h host -U user -D /var/lib/postgresql/data -v -P -X s -c fast
-
-primary_conninfo = 'host=host port=5432 user=user password=password'
+! postgresql.conf
+primary_conninfo = 'host=db-master port=5432 user=repuser password=repuser'	
+primary_slot_name = 'master_replication_slot'	
